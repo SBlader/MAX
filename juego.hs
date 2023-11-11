379e879bbf3f507ec58e6856873b0c8361aa41b3
benrriXWorks <BenrriXWorks@gmail.com>
2023-11-11 20:40:52 -0300

    Main mejorado y ordenado

--- juego.hs
+++ juego.hs
@@ -1,66 +1,78 @@
-import System.Process
-import System.Random
+import System.Process ( callCommand )
+import System.Random ( mkStdGen )
 import MapGen
-import System.Environment
+import System.Environment ( getArgs )
+import System.IO ( hSetBuffering, stdin, BufferMode(NoBuffering) ) 
 
-clearScreen :: IO ()
-clearScreen = callCommand "clear"
+-- Estructura para recibir la entrada
+data Action k = Action {getMov::(Int,Int), getKey::k}
 
+-- Main inicializa los argumentos y el mapa
 main :: IO ()
 main = do
+    -- Desactivar el buffering de entrada (getchar Instantaneo)
+    hSetBuffering stdin NoBuffering
+    -- Leer los argumentos
     args <- getArgs
-    clearScreen
-    putStrLn "___  ___  ___  __   __"
-    putStrLn "|  \\/  | / _ \\ \\ \\ / /"
-    putStrLn "| .  . |/ /_\\ \\ \\ V /" 
-    putStrLn "| |\\/| ||  _  | /   \\" 
-    putStrLn "| |  | || | | |/ /^\\ \\"
-    putStrLn "\\_|  |_/\\_| |_/\\/   \\/"
-    putStrLn "Coloque un n:"
-    let nStr = head args
-    let numeroRandom = read (args !! 1) :: Int
-    let rand = mkStdGen numeroRandom
-    let n = (+(-1)) $ (read nStr :: Int)
-    let cantidadPozos = round (1.5 * fromIntegral n) :: Int
-    let cantidadObstaculos = 3 * round (1.5 * fromIntegral n) :: Int
-    putStrLn "Mapa actualizado: "
-    let mapa = generarMapaCaminable (n+1) (n+1)
-    let mapaConObstaculos = foldr (\(x, y) acc -> cambiarCelda acc (x, y) Obstaculo) mapa (genChunk (n,n) [] cantidadObstaculos rand chunksObstaculos)
-    let mapaConLava = foldr (\(x, y) acc -> cambiarCelda acc (x, y) Lava) mapaConObstaculos (genChunk (n, n) [] cantidadPozos (nextRandom rand) chunksLava) 
-    loop mapaConLava (0, 0) n n
+    if length args /= 2 then 
+        putStrLn "Debes ingresar los argumentos (n::Tamano mapa) y (r::Int generador)"
+    else do
+        clearScreen
+        print args
+        -- Leer parametros
+        let rand = mkStdGen (read $ last args :: Int) -- Generador con el numero random
+        let n = (read $ args !! 0 :: Int) -- Largo mapa
+        let cantidadPozos = round (1.5 * fromIntegral n) :: Int
+        let cantidadObstaculos = 3 * cantidadPozos :: Int
+        -- Ahora se genera mapa vacio, luego se le colocan obstaculos y por ultimo lava
+        let mapaVacio = generarMapaCaminable (n) (n)
+        let mapaConObstaculos = foldr (\(x, y) acc -> cambiarCelda acc (x, y) Obstaculo) mapaVacio (genChunk (n, n) [] cantidadObstaculos rand chunksObstaculos)
+        let mapaConLava = foldr (\(x, y) acc -> cambiarCelda acc (x, y) Lava) mapaConObstaculos (genChunk (n, n) [] cantidadPozos (nextRandom rand) chunksLava) 
+        loop mapaConLava (0, 0) (n) n -- Iniciar Loop
 
+
+-- Funciones auxiliares
+-- Gameloop: Dibujar, Esperar movimiento, Cambiar estado y repetir
 loop :: Mapa Celda -> (Int, Int) -> Int -> Int -> IO ()
 loop mapa (x,y) n m = do
     clearScreen
-    let playerMapa = cambiarCelda mapa (x,y) Jugador
-    print playerMapa
-    mov <- getChar
-    let newPos = case mov of
-            'w' -> (x, max 0 (min m (y - 1)))
-            's' -> (x, max 0 (min m (y + 1))) 
-            'a' -> (max 0 (min n (x - 1)), y)
-            'd' -> (max 0 (min n (x + 1)), y)
-            _   -> (x,y)
-            -- r -> reiniciar el mapa
-    -- if mapa[head newPos][last newPos] == Lava Then Chao ctm   
-    if obtenerCelda mapa newPos == Lava
-        then do
-            clearScreen
-            putStrLn "@@@ @@@   @@@@@@   @@@  @@@     @@@@@@@   @@@  @@@@@@@@  @@@@@@@   @@@  @@@  "
-            putStrLn "@@@ @@@  @@@@@@@@  @@@  @@@     @@@@@@@@  @@@  @@@@@@@@  @@@@@@@@  @@@  @@@  "
-            putStrLn "@@! !@@  @@!  @@@  @@!  @@@     @@!  @@@  @@!  @@!       @@!  @@@  @@!  @@!"
-            putStrLn "!@! @!!  !@!  @!@  !@!  @!@     !@!  @!@  !@!  !@!       !@!  @!@  !@   !@   "
-            putStrLn " !@!@!   @!@  !@!  @!@  !@!     @!@  !@!  !!@  @!!!:!    @!@  !@!  @!@  @!@  "
-            putStrLn "  @!!!   !@!  !!!  !@!  !!!     !@!  !!!  !!!  !!!!!:    !@!  !!!  !!!  !!!  "
-            putStrLn "  !!:    !!:  !!!  !!:  !!!     !!:  !!!  !!:  !!:       !!:  !!!"            
-            putStrLn "  :!:    :!:  !:!  :!:  !:!     :!:  !:!  :!:  :!:       :!:  !:!  :!:  :!:"  
-            putStrLn "  ::    ::::: ::  ::::: ::      :::: ::   ::   :: ::::   :::: ::   ::   ::" 
-            putStrLn "  :      : :  :    : :  :      :: :  :   :    : :: ::   :: :  :   :::  :::"
-    else if obtenerCelda mapa newPos == Obstaculo
-            then loop mapa (x,y) n m
-    else do
-        let newMapa = cambiarCelda mapa (x,y) Camino
-        loop newMapa newPos n m
+    print (cambiarCelda mapa (x,y) Jugador)
+    -- Iniciar acciones segun el movimiento
+    key <- getChar
+    let (action, newCelda) = (getAction (x,y) n m key, obtenerCelda mapa $ getMov action) in 
+        if (getKey action == 'r') then print "RESETEAR jaja"
+        else if newCelda == Lava then deadMessage -- Si vas a caminar en lava        
+        else if newCelda == Obstaculo then loop mapa (x,y) n m -- Si vas a caminar en un obstaculo
+        else loop (cambiarCelda mapa (x,y) Camino) (getMov action) n m -- Si vas a caminar sobre un suelo caminable
 
+-- Funciones auxiliares
+getAction :: (Int, Int) -> Int -> Int -> Char -> Action Char
+getAction (x,y) n m key 
+    | key == 'w' = Action (x, max 0 (min m (y - 1))) 'w'
+    | key == 'a' = Action (max 0 (min n (x - 1)), y) 'a'
+    | key == 's' =  Action (x, max 0 (min m (y + 1)))  's'
+    | key == 'd' = Action (max 0 (min n (x + 1)), y) 'd'
+    | otherwise = Action (x,y) key
+
+-- Obtiene el contenido de una celda del mapa
 obtenerCelda :: Mapa Celda -> (Int, Int) -> Celda
 obtenerCelda (Mapa matriz) (x, y) = (matriz !! y) !! x
+
+-- Limpia la pantalla
+clearScreen :: IO ()
+clearScreen = callCommand "clear"
+
+-- Devuelve el mensaje de muerte
+deadMessage :: IO ()
+deadMessage = do
+    clearScreen
+    putStrLn "@@@ @@@   @@@@@@   @@@  @@@     @@@@@@@   @@@  @@@@@@@@  @@@@@@@   @@@  @@@  "
+    putStrLn "@@@ @@@  @@@@@@@@  @@@  @@@     @@@@@@@@  @@@  @@@@@@@@  @@@@@@@@  @@@  @@@  "
+    putStrLn "@@! !@@  @@!  @@@  @@!  @@@     @@!  @@@  @@!  @@!       @@!  @@@  @@!  @@!"
+    putStrLn "!@! @!!  !@!  @!@  !@!  @!@     !@!  @!@  !@!  !@!       !@!  @!@  !@   !@   "
+    putStrLn " !@!@!   @!@  !@!  @!@  !@!     @!@  !@!  !!@  @!!!:!    @!@  !@!  @!@  @!@  "
+    putStrLn "  @!!!   !@!  !!!  !@!  !!!     !@!  !!!  !!!  !!!!!:    !@!  !!!  !!!  !!!  "
+    putStrLn "  !!:    !!:  !!!  !!:  !!!     !!:  !!!  !!:  !!:       !!:  !!!"            
+    putStrLn "  :!:    :!:  !:!  :!:  !:!     :!:  !:!  :!:  :!:       :!:  !:!  :!:  :!:"  
+    putStrLn "  ::     ::::: ::  ::::: ::      :::: ::   ::   :: ::::   :::: ::   ::   ::" 
+    putStrLn "   :      : :  :    : :  :      :: :  :   :    : :: ::   :: :  :   :::  :::"
